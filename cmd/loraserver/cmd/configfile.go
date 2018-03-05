ef5e48ab12c17d7aa1c6780868f3d89f67a4b621
Orne Brocaar <info@brocaar.com>
2018-03-05 11:37:59 +0100

    Expose MQTT client id, qos and clean-session options.
    Closes #60.

--- cmd/loraserver/cmd/configfile.go
+++ cmd/loraserver/cmd/configfile.go
@@ -335,6 +335,30 @@ get_downlink_data_delay="{{ .NetworkServer.GetDownlinkDataDelay }}"
   # Connect with the given password (optional)
   password="{{ .NetworkServer.Gateway.Backend.MQTT.Password }}"
 
+  # Quality of service level
+  #
+  # 0: at most once
+  # 1: at least once
+  # 2: exactly once
+  #
+  # Note: an increase of this value will decrease the performance.
+  # For more information: https://www.hivemq.com/blog/mqtt-essentials-part-6-mqtt-quality-of-service-levels
+  qos={{ .NetworkServer.Gateway.Backend.MQTT.QOS }}
+
+  # Clean session
+  #
+  # Set the "clean session" flag in the connect message when this client
+  # connects to an MQTT broker. By setting this flag you are indicating
+  # that no messages saved by the broker for this client should be delivered.
+  clean_session={{ .NetworkServer.Gateway.Backend.MQTT.CleanSession }}
+
+  # Client ID
+  #
+  # Set the client id to be used by this client when connecting to the MQTT
+  # broker. A client id must be no longer than 23 characters. When left blank,
+  # a random id will be generated. This requires clean_session=true.
+  client_id="{{ .NetworkServer.Gateway.Backend.MQTT.ClientID }}"
+
   # CA certificate file (optional)
   #
   # Use this when setting up a secure connection (when server uses ssl://...)
